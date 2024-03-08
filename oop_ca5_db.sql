ae578fe13aff97c039984d8b93e3682bd65a5d45
Jakub Polacek <Kuba.krtek@email.cz>
2024-03-08 03:50:08 +0100

    FInished intial SQL db file

--- oop_ca5_db.sql
+++ oop_ca5_db.sql
@@ -7,8 +7,8 @@ USE MC_items;
 CREATE TABLE blocks 
 (block_id INTEGER not NULL AUTO_INCREMENT, 
  block_name VARCHAR(20), 
- hardness INTEGER not NULL, 
- blast_resistance INTEGER not NULL, 
+ hardness DOUBLE(4,2), 
+ blast_resistance DOUBLE(8,2), 
  gravity_affected BOOLEAN,
  PRIMARY KEY (block_id));
  
@@ -17,5 +17,16 @@ CREATE TABLE blocks
 
 /* BLOCKS */
 INSERT INTO blocks (block_id,block_name,hardness,blast_resistance,gravity_affected)
-VALUES (),
-
+VALUES (1,'cobblestone',2,6,FALSE),
+ (2,'grass block',0.6,0.6,FALSE),
+ (3,'sand',0.5,0.5,TRUE),
+ (4,'obsidian',50,1200,FALSE),
+ (5,'log',2,2,FALSE),
+ (6,'stone',1.5,6,FALSE),
+ (7,'gravel',0.6,0.6,TRUE),
+ (8,'terracota',1.25,4.2,FALSE),
+ (9,'clay',0.6,0.6,FALSE),
+ (10,'bricks',2,6,FALSE),
+ (11,'sandstone',0.8,0.8,FALSE),
+ (12,'prismarine',1.5,6,FALSE),
+ (13,'diamond block',5,6,FALSE);
